0e252d63d930f6e18814bf90bf38627366788208
luqmanmatloob <luqman8596@gmail.com>
2024-12-05 23:59:33 +0500

    darkmode toggle fixed

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
-import { ModeToggle } from "@/components/ModeToggle";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
-<ModeToggle />
+    <div className="
+flex gap-20  items-center justify-center min-h-screen py-2
+">
+      <Button>
+        test
+      </Button>
+
+      <button> hello</button>
+
+      <button className="bg-black  text-white dark:bg-white dark:text-black px-4 py-2 rounded-md shadow-md hover:bg-gray-800 dark:hover:bg-gray-200 transition-colors">
+        hello
+      </button>
+
     </div>
   );
 }
--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
-import {ModeToggle} from '../ModeToggle';
+import { ModeToggle } from '../ModeToggle';
 
 const Header = () => {
-  return (
-    <header className="bg-blue-600 text-white p-4">
-      <nav className="container mx-auto flex justify-between items-center">
-        <a href="/" className="text-2xl font-semibold">My Blog</a>
-        <ul className="flex space-x-6">
-          <li><a href="/" className="hover:text-gray-200">Home</a></li>
-          <li><a href="/about" className="hover:text-gray-200">About</a></li>
-          <li><a href="/contact" className="hover:text-gray-200">Contact</a></li>
-          <li className="text-black dark:text-white">
-  <ModeToggle />
-</li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className="bg-blue-600 text-white p-4">
+            <nav className="container mx-auto flex justify-between items-center">
+                <a href="/" className="text-2xl font-semibold">My Blog</a>
+                <ul className="flex space-x-6">
+                    <li><a href="/" className="hover:text-gray-200">Home</a></li>
+                    <li><a href="/about" className="hover:text-gray-200">About</a></li>
+                    <li><a href="/contact" className="hover:text-gray-200">Contact</a></li>
+                    <li className=
+                    "bg-white text-black  dark:bg-blue-600 dark:text-white"
+                    >
+
+                        <ModeToggle />
+                    </li>
+                    
+                    
+
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
