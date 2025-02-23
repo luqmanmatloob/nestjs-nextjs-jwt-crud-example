7afb551b454cbca0c8f1a9bcd7457218310ea17a
Luqman Matloob <93182189+luqmanmatloob@users.noreply.github.com>
2025-02-23 21:19:47 +0500

    client : updated

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
+import React from "react";
+import { Button } from "@/components/ui/button";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white p-4 mt-8">
+    <footer className="border-t p-4 mt-8">
       <div className="container mx-auto text-center">
         <p>&copy; {new Date().getFullYear()} My Blog. All Rights Reserved.</p>
-        <ul className="flex justify-center space-x-6 mt-4">
-          <li><a href="/privacy" className="hover:text-gray-400">Privacy Policy</a></li>
-          <li><a href="/terms" className="hover:text-gray-400">Terms of Service</a></li>
-        </ul>
       </div>
     </footer>
   );
