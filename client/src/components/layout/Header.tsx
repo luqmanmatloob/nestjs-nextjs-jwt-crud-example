1cec2fc04a1e9270e95e595d6cf7054c4522de3e
Luqman Matloob <93182189+luqmanmatloob@users.noreply.github.com>
2025-02-23 21:17:04 +0500

    client : updated

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,28 +1,37 @@
-import React from 'react';
-import { ModeToggle } from '../ModeToggle';
+import React from "react";
+import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
-    return (
-        <header className="bg-blue-600 text-white p-4">
-            <nav className="container mx-auto flex justify-between items-center">
-                <a href="/" className="text-2xl font-semibold">My Blog</a>
-                <ul className="flex space-x-6">
-                    <li><a href="/signup" className="hover:text-gray-200">sign up</a></li>
-                    <li><a href="/login" className="hover:text-gray-200">login</a></li>
-                    <li><a href="/create-post" className="hover:text-gray-200">create post</a></li>
-                    <li className=
-                    "bg-white text-black  dark:bg-blue-600 dark:text-white"
-                    >
-
-                        <ModeToggle />
-                    </li>
-                    
-                    
-
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="border-b p-4">
+      <nav className="container mx-auto flex justify-between items-center">
+        <a href="/" className="text-2xl font-semibold">
+          My Blog
+        </a>
+        <ul className="flex space-x-6">
+          <li className="flex items-center">
+            <Button asChild variant="ghost" >
+              <a href="/signup">Sign Up</a>
+            </Button>
+          </li>
+          <li className="flex items-center">
+            <Button asChild variant="ghost">
+              <a href="/login">Login</a>
+            </Button>
+          </li>
+          <li className="flex items-center">
+            <Button asChild variant="ghost">
+              <a href="/create-post">Create Post</a>
+            </Button>
+          </li>
+          <li className="p-2 rounded-md ">
+            <ModeToggle />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
